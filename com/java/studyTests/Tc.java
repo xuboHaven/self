b2e31ee6865ed357e06d8eca277b61391b321c00
xuboHaven <1240886023@qq.com>
2022-03-20 11:00:21 +0800

    testCommit

--- com/java/studyTests/Tc.java
+++ com/java/studyTests/Tc.java
@@ -2,6 +2,6 @@ package studyTests;
 
 public class Tc {
     public static void main(String[] args) {
-        System.out.println("HELLO WORLD");
+        System.out.println("HELLO WORLD22");
     }
 }
